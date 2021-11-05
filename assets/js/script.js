f975d6ef70075771118881a09824e9a840b00c53
Molly Montgomery <Molly@mollys-air-2.lan>
2021-11-05 12:05:49 -0500

    added all trivia questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,9 @@ var questions = [{
     answer: "c. James & Lily"
 },
 {
-    title: " Which built-in method returns the characters in a string beginning at the specified location?",
-    choices: ["substr( )", "getSubstring( )", "slice( )", "None of the above."],
-    answer: "substr( )"
+    title: "What position does Harry play on his Qudditch team?",
+    choices: ["a. Keeper", "b. Bludger", "c. Chaser", "d. Seeker"],
+    answer: "d. Seeker"
 },
 {
     title: "What Hogwarts house does Cedric Diggory belong to?",
@@ -23,6 +23,31 @@ var questions = [{
     title: "What's Voldemorts real name?",
     choices: ["a. Tim Roddle", "b. Ron Timble", "c. Thomas Raddle", "d. Tom Riddle"],
     answer: "d. Tom Riddle"
+},
+{
+    title: "What power do the dementors have over people?",
+    choices: ["a. The cause them to harm one another", "b. They drain them of all happiness", "c. The make them do their bidding", "d. They make them go crazy"],
+    answer: "b. The drain them of all happiness"
+},
+{
+    title: "Who kills Professor Dumbledore?",
+    choices: ["a. Severus Snape", "b. Bellatrix LeStrange", "c. Draco Malfoy", "d. Lucius Malfoy"],
+    answer: "a. Severus Snape"
+},
+{
+    title: "How does Harry catch his first snitch?",
+    choices: ["a. With his hands", "b. With his feet", "c. In his mouth", "d. In his pocket"],
+    answer: "c. In his mouth"
+},
+{
+    title: "How are Hogwarts students placed in their houses?",
+    choices: ["a. They're assigned by a professor", "b. They draw straws", "c. They are in the same house as their parents", "d. The Sorting Hat"],
+    answer: "d. The Sorting Hat"
+},
+{
+    title: "What does the Marauder's Map show?",
+    choices: ["a. The location of everyone at Hogwarts", "b. The Room of Requirement", "c. The Password to Dumbledore's office", "d. Hidden Treasure"],
+    answer: "a. The location of everyone at Hogwarts"
 }
 ]
 
