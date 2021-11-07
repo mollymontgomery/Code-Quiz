13d1a9a2d2446b3055411a92b9bc865d8dd43ed7
Molly Montgomery <Molly@mollys-air-2.lan>
2021-11-07 12:29:10 -0600

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,9 +102,7 @@ function getScore() {
 var quizContent = `
 <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
 <h1>` + localStorage.getItem("highscore") + `</h1><br> 
-
 <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
-
 `;
 
 document.getElementById("quizBody").innerHTML = quizContent;
